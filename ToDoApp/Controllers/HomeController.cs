5fbc92435d488b6774f9a5820fac4385e3cf86a5
Batuhan <anhtubui574@gmail.com>
2024-08-27 15:49:55 +0700

    added SelectList The property CategoryName should match exactly what is defined in model class.

--- ToDoApp/Controllers/HomeController.cs
+++ ToDoApp/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
 using ToDoApp.Models;
 
 namespace ToDoApp.Controllers
@@ -10,10 +9,7 @@ namespace ToDoApp.Controllers
         private ToDoContext _context;
         //private readonly ILogger<HomeController> _logger;
 
-        //public HomeController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
+
         public HomeController(ToDoContext context) => _context = context;
 
         public IActionResult Index(string id)
@@ -28,39 +24,40 @@ namespace ToDoApp.Controllers
                 .Include(t => t.Category)
                 .Include(t => t.Status);
 
-            if(filters.HasCategory)
+            if (filters.HasCategory)
             {
                 query = query.Where(t => t.CategoryId == filters.CategoryId);
             }
-            
-            if(filters.HasStatus)
+
+            if (filters.HasStatus)
             {
                 query = query.Where(t => t.StatusId == filters.StatusId);
             }
 
-            if(filters.HasDue)
+            if (filters.HasDue)
             {
                 var today = DateTime.Today;
-                if(filters.IsPast)
+                if (filters.IsPast)
                 {
                     query = query.Where(t => t.DueTime < today);
                 }
-                else if(filters.IsToday)
+                else if (filters.IsToday)
                 {
                     query = query.Where(t => t.DueTime == today);
                 }
-                else if(filters.IsFuture)
+                else if (filters.IsFuture)
                 {
                     query = query.Where(t => t.DueTime > today);
                 }
             }
-            
+
             var tasks = query.OrderBy(t => t.DueTime).ToList();
 
             return View(tasks);
         }
 
-        [HttpGet]
+
+		[HttpGet]
         public IActionResult Add()
         {
             ViewBag.Categories = _context.Categories.ToList();
@@ -72,7 +69,7 @@ namespace ToDoApp.Controllers
         [HttpPost]
         public IActionResult Add(ToDo task)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _context.ToDos.Add(task);
                 _context.SaveChanges();
@@ -86,16 +83,46 @@ namespace ToDoApp.Controllers
             }
         }
 
+        [HttpPost]
+        public IActionResult Filter(string[] filter)
+        {
+            var id = string.Join('-', filter);
+            return RedirectToAction("Index", new { Id = id });
+        }
 
-        //public IActionResult Privacy()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        // FromRoute attribute: hey get the id from the URl and pass it to the method
+        public IActionResult MarkComplete([FromRoute] string id, ToDo selected)
+        {
+            selected = _context.ToDos.Find(selected.Id);
+            if (selected != null)
+            {
+                selected.StatusId = "closed";
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index", new { Id = id });
+        }
 
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //}
+        public IActionResult DeleteCompleted([FromRoute] string id)
+        {
+            var toDelete = _context.ToDos.Where(t => t.StatusId == "closed").ToList();
+            foreach (var todo in toDelete)
+            {
+                _context.ToDos.Remove(todo);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", new { Id = id });
+        }
+
+            //public IActionResult Privacy()
+            //{
+            //    return View();
+            //}
+
+            //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            //public IActionResult Error()
+            //{
+            //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            //}
+        }
     }
-}
